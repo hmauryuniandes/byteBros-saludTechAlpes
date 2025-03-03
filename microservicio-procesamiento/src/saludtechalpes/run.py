6dfa1a129e1d90d941bb3ac8c458b5872b667bad
Andres Lombo <andreslombo@ingeted.com.co>
2025-03-03 12:38:57 -0500

    db init incluida

--- microservicio-procesamiento/src/saludtechalpes/run.py
+++ microservicio-procesamiento/src/saludtechalpes/run.py
@@ -1,7 +1,8 @@
 from src.saludtechalpes.presentacion.api import app
-
+from src.saludtechalpes.config.db import init_db
 from src.saludtechalpes.modulos.anonimizacion.infraestructura.consumidores import iniciar_consumidor
 
 if __name__ == '__main__':
+    init_db()
     iniciar_consumidor()
     app.run(host='0.0.0.0', port=5000, debug=True)
